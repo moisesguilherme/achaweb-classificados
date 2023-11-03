7b3a1183e693dfb2d6a592aa0687740ae3af2e12
Moisés Guilherme <moisescomp@gmail.com>
2023-11-02 21:19:33 -0300

    Initial project with spring

---
+++ backend/src/main/java/br/com/achaweb/achaweb/AchawebApplication.java
@@ -0,0 +1,13 @@
+package br.com.achaweb.achaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchawebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/achaweb/achaweb/AchawebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.achaweb.achaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
