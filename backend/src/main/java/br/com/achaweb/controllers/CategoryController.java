f78d1bb71f38e5b537bb68a7ced4809f4c75ecd5
Moisés Guilherme <moisescomp@gmail.com>
2023-11-03 21:24:35 -0300

    Add DTO pattern

--- backend/src/main/java/br/com/achaweb/controllers/CategoryController.java
+++ backend/src/main/java/br/com/achaweb/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package br.com.achaweb.controllers;
 
+import br.com.achaweb.dto.CategoryDTO;
 import br.com.achaweb.entities.Category;
 import br.com.achaweb.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ public class CategoryController {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
--- backend/src/main/java/br/com/achaweb/services/CategoryService.java
+++ backend/src/main/java/br/com/achaweb/services/CategoryService.java
@@ -1,10 +1,13 @@
 package br.com.achaweb.services;
 
+import br.com.achaweb.dto.CategoryDTO;
 import br.com.achaweb.entities.Category;
 import br.com.achaweb.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +16,15 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<Category> findAll() {
-        return repository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+
+        List<CategoryDTO> listDto = new ArrayList<>();
+        for (Category category : list) {
+            listDto.add(new CategoryDTO(category));
+        }
+        return listDto;
     }
 
 }
