c6f527a03b22be866b0b388cea8b80f6455bf954
Moisés Guilherme <moisescomp@gmail.com>
2024-06-18 07:47:44 -0300

    Custom ConstraintValidator

--- backend/src/main/java/br/com/achaweb/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/br/com/achaweb/services/validation/UserUpdateValidator.java
@@ -6,13 +6,19 @@ import br.com.achaweb.dto.UserUpdateDTO;
 import br.com.achaweb.entities.User;
 import br.com.achaweb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
 
     @Autowired
     private UserRepository repository;
@@ -21,14 +27,17 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
     public void initialize(UserUpdateValid ann) {
     }
 
-
+    @Override
     public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+
+        @SuppressWarnings("unchecked")
+        var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        long userId = Long.parseLong(uriVars.get("id"));
 
         List<FieldMessage> list = new ArrayList<>();
 
         User user = repository.findByEmail(dto.getEmail());
-
-        if(user != null) {
+        if(user != null && userId != user.getId()) {
             list.add(new FieldMessage("email", "Email já existe"));
         }
 
